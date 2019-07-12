c0ea52459ca4c596f45a1f59e02822c9d08b2168
denmercs <denmercs@gmail.com>
2019-07-12 12:52:46 -0500

    done with sprint

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,57 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+class Carousel {
+  constructor(carousel) {
+    this.carousel = carousel;
+    // 1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
+    this.rightBtn = this.carousel.querySelector('.right-button');
+    this.leftBtn = this.carousel.querySelector('.left-button');
+
+    // 2. You will need to grab a reference to all of the images
+    this.images = this.carousel.querySelectorAll('img');
+
+    // 3. Create a current index
+    this.index = 0;
+
+    // 4. Those buttons are gonna need some click handlers.
+    this.leftBtn.addEventListener('click', () => this.moveLeft());
+    this.rightBtn.addEventListener('click', () => this.moveRight());
+
+  }
+
+  moveLeft() {
+    this.images.forEach(img => img.classList.remove('img-show'));
+    this.index--;
+    if (this.index < 0) {
+      this.index = this.images.length - 1;
+    }
+    this.images[this.index].classList.add('img-show');
+  }
+
+  moveRight() {
+    this.images.forEach(img => img.classList.remove("img-show"));
+    this.index++;
+    if (this.index > this.images.length - 1) {
+      this.index = 0;
+    }
+    this.images[this.index].classList.add("img-show");
+  }
+
+  animate() {
+
+  }
+}
+
+let carousel = document.querySelector('.carousel-container');
+carousel = new Carousel(carousel);
+
+/* If You've gotten this far, you're on your own! Although we will give you some hints:
+  
+  
+  3. Create a current index
+  4. Those buttons are gonna need some click handlers.
+  5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
+  6. Have fun!
+*/
